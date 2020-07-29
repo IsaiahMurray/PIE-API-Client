c63d380249fb52199f9eeb3d3660881cd7f1a445
Isaiah Murray <murray.isaiah13@gmail.com>
2020-07-29 13:33:02 -0400

    made a bunch of pie files

---
+++ src/components/Pies/Pies.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import './Pies.css';
+
+const Pies = (props) => {
+
+    const [pies, setPies] = useState([])
+
+    
+
+}
+
+export default Pies;
